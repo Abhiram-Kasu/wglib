c077625c884f78eedf97c0ebdcd5e0fb235e9480
Abhiram Kasu <abhiram.kasu@gmail.com>
2025-12-09 11:25:16 -0700

    Changed Rectangle render layer to have one static render pipeline

--- src/lib/render_layer/rectangle_render_layer.hpp
+++ src/lib/render_layer/rectangle_render_layer.hpp
@@ -9,29 +9,33 @@
 #include "webgpu/webgpu_cpp.h"
 
 namespace wglib::render_layers {
-class RectangleRenderLayer : public RenderLayer {
-private:
-  wgpu::RenderPipeline m_render_pipeline;
+    class RectangleRenderLayer : public RenderLayer {
+    private:
+        static std::optional<wgpu::RenderPipeline> m_render_pipeline;
 
-  glm::vec2 m_size;
-  glm::vec2 m_position;
-  glm::vec3 m_color;
+        glm::vec2 m_size;
+        glm::vec2 m_position;
+        glm::vec3 m_color;
 
-  wgpu::Buffer m_vertex_buffer;
-  wgpu::Buffer m_index_buffer;
+        wgpu::Buffer m_vertex_buffer;
+        wgpu::Buffer m_index_buffer;
 
-  Vertex m_vertices[6];
+        Vertex m_vertices[6];
 
-  auto initVertices() -> void;
+        auto initVertices() -> void;
 
-public:
-  RectangleRenderLayer(glm::vec2 position, glm::vec2 size, glm::vec3 color);
+        static auto initRenderPipeline(const wgpu::Device &,
+                                       wgpu::TextureFormat,
+                                       const wgpu::BindGroupLayout &) -> void;
 
-  auto initRes(const wgpu::Device &device, wgpu::TextureFormat format,
-               const wgpu::BindGroupLayout &bindGroupLayout) -> void override;
+    public:
+        RectangleRenderLayer(glm::vec2 position, glm::vec2 size, glm::vec3 color);
 
-  auto Render(wgpu::RenderPassEncoder &) -> void override;
+        auto initRes(const wgpu::Device &device, wgpu::TextureFormat format,
+                     const wgpu::BindGroupLayout &bindGroupLayout) -> void override;
 
-  ~RectangleRenderLayer() override;
-};
+        auto Render(wgpu::RenderPassEncoder &) -> void override;
+
+        ~RectangleRenderLayer() override;
+    };
 } // namespace wglib::render_layers
